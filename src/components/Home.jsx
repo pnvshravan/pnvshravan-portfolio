484293da2ad759e8f9cdfbdb8bab80301a991b92
pnvshravan <pnvshravan786@outlook.com>
2025-01-26 16:55:39 +0530

    change img state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home=()=>{
               flex pt-6 justify-center mob-sm:pt-10">
                 <div className="flex flex-row gap-10 place-items-center w-[40rem] mob-sm:flex-col mob-sm:w-[15rem] mob-sm:gap-5 tablet:flex-col tablet:w-[25rem] ">
                     <div className=" w-auto h-auto rounded-md hover:ring-gray-400 hover:ring-3">
-                        <img src={self} alt="self-photo" height={100} width={150} className="rounded"/>
+                        <img src={self} alt="self-photo" height={100} width={150} className="rounded" loading="lazy"/>
                     </div>    
                     <div>
                         <div className="mb-2 rounded-md bg-[#233A6C] p-2 max-w-[30rem] hover:ring-3 hover:ring-teal-400 mob-sm:w-[15rem] ">
